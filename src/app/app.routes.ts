12ed918b8c4d0ab987b2c2b3bc289dbca8b392b2
Eric-Obeng <obengeric.3958@gmail.com>
2025-06-24 09:53:04 +0000

    feat: implement routing for home page and include header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('../app/pages/home-page/home-page').then((m) => m.HomePage),
+  },
+];
