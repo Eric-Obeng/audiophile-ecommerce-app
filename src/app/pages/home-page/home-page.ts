dc15e61e095e6205e0594f9c1d665c96b79732fc
Eric-Obeng <obengeric.3958@gmail.com>
2025-06-25 10:17:36 +0000

    feat: update home page structure and integrate HeroSection component

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeroSection } from "../../shared/components/hero-section/hero-section";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [HeroSection],
   templateUrl: './home-page.html',
   styleUrl: './home-page.sass'
 })
