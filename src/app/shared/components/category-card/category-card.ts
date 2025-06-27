7fae1a2d20da9172302445cb09f8a5af122c9425
Eric-Obeng <obengeric.3958@gmail.com>
2025-06-27 16:12:57 +0000

    feat: enhance category card with routing and link styling - Added routing to the category link in the category card component. - Updated the SASS file to ensure the link has no text decoration.

--- src/app/shared/components/category-card/category-card.ts
+++ src/app/shared/components/category-card/category-card.ts
@@ -1,8 +1,9 @@
 import { Component, input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-category-card',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './category-card.html',
   styleUrl: './category-card.sass',
 })
@@ -10,4 +11,8 @@ export class CategoryCard {
   categoryName = input.required<string>();
   categoryImage = input.required<string>();
   categoryLink = input.required<string>();
+
+  get categoryRoute(): string[] {
+    return ['/category', this.categoryName().toLowerCase()];
+  }
 }
