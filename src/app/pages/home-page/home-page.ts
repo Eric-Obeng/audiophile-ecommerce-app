d647fea84e459d7414b3a993e7e8b93eac890ad0
Eric-Obeng <obengeric.3958@gmail.com>
2025-06-27 08:15:52 +0000

    style: enhance home page layout with animations and responsive design

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { HeroSection } from "../../shared/components/hero-section/hero-section";
+import { HeroSection } from '../../shared/components/hero-section/hero-section';
+import { CategoryMenu } from '../../shared/components/category-menu/category-menu';
+import { ScrollAnimationDirective } from '../../shared/directives/scroll-animation.directive';
+import { Footer } from "../../shared/components/footer/footer";
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeroSection],
+  imports: [HeroSection, CategoryMenu, ScrollAnimationDirective, Footer],
   templateUrl: './home-page.html',
-  styleUrl: './home-page.sass'
+  styleUrl: './home-page.sass',
 })
-export class HomePage {
-
-}
+export class HomePage {}
