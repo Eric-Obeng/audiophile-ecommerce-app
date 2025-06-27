23d56da66451e1abe61eaf15d8036dc66e6597df
Eric-Obeng <obengeric.3958@gmail.com>
2025-06-27 16:10:14 +0000

    feat: enhance app configuration with routing features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,15 @@ export const routes: Routes = [
     loadComponent: () =>
       import('../app/pages/home-page/home-page').then((m) => m.HomePage),
   },
+  {
+    path: 'category/:categoryName',
+    loadComponent: () =>
+      import('../app/pages/category-page/category-page').then(
+        (m) => m.CategoryPage
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
