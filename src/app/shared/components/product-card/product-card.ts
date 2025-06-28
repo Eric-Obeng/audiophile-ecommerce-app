5f6c855d107b3456cb2ca4aae2c1c7db0f19a2a3
Eric-Obeng <obengeric.3958@gmail.com>
2025-06-28 15:57:10 +0000

    feat: add product card component with responsive design and image handling

---
+++ src/app/shared/components/product-card/product-card.ts
@@ -0,0 +1,40 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { CompleteProduct } from '../../../core/models/products.model';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [NgOptimizedImage, RouterLink],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.sass',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductCard {
+  product = input.required<CompleteProduct>();
+
+  findCategoryImage(
+    product: CompleteProduct,
+    type: 'mobile' | 'tablet' | 'desktop' = 'mobile'
+  ): string {
+    if (!product.images || product.images.length === 0) {
+      return `assets/shared/${type}/image-category-thumbnail-fallback.png`;
+    }
+
+    const categoryImage = product.images.find((img) => img.type === 'category');
+
+    if (!categoryImage) {
+      return `assets/shared/${type}/image-category-thumbnail-fallback.png`;
+    }
+
+    switch (type) {
+      case 'mobile':
+        return categoryImage.mobile_url;
+      case 'tablet':
+        return categoryImage.tablet_url;
+      case 'desktop':
+      default:
+        return categoryImage.mobile_url;
+    }
+  }
+}
