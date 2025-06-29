e74cbb20b5a84dd8421b0757dca5958af0bdb038
Eric-Obeng <obengeric.3958@gmail.com>
2025-06-29 21:04:05 +0000

    add signup initial component

---
+++ src/app/pages/auth/signup/signup.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  imports: [],
+  templateUrl: './signup.html',
+  styleUrl: './signup.sass'
+})
+export class Signup {
+
+}
