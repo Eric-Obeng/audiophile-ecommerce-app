40858d3d6f4145eea8ee064df30f6ecb15a31e0b
Eric-Obeng <obengeric.3958@gmail.com>
2025-07-04 23:24:00 +0000

    feat: implement product detail layout with category menu, info, and footer

--- src/app/pages/product-detail-page/product-detail-page.ts
+++ src/app/pages/product-detail-page/product-detail-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CategoryMenu } from "../../shared/components/category-menu/category-menu";
+import { Info } from "../../shared/components/info/info";
+import { Footer } from "../../shared/components/footer/footer";
 
 @Component({
   selector: 'app-product-detail-page',
-  imports: [],
+  imports: [CategoryMenu, Info, Footer],
   templateUrl: './product-detail-page.html',
   styleUrl: './product-detail-page.sass'
 })
