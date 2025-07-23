ce47be7d9e9700ff4a826899ffc14bad1282db61
Eric-Obeng <obengeric.3958@gmail.com>
2025-07-23 13:15:36 +0000

    feat: implement checkout form with dynamic validation Added a checkout form with billing, shipping, and payment details, including dynamic validation for e-money fields based on the selected payment method.

--- src/app/pages/checkout-page/checkout-page.ts
+++ src/app/pages/checkout-page/checkout-page.ts
@@ -1,11 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthInput } from '../../shared/components/auth-input/auth-input';
+import { Footer } from '../../shared/components/footer/footer';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-checkout-page',
-  imports: [],
+  imports: [AuthInput, Footer, ReactiveFormsModule, CommonModule],
   templateUrl: './checkout-page.html',
-  styleUrl: './checkout-page.sass'
+  styleUrl: './checkout-page.sass',
 })
 export class CheckoutPage {
+  readonly fb = inject(FormBuilder);
 
+  checkoutForm: FormGroup = this.fb.group({
+    billingDetails: this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required]],
+    }),
+
+    shippingDetails: this.fb.group({
+      address: ['', [Validators.required]],
+      zipCode: ['', [Validators.required, Validators.minLength(5)]],
+      city: ['', [Validators.required]],
+      country: ['', [Validators.required]],
+    }),
+
+    paymentDetails: this.fb.group({
+      method: ['e-money', Validators.required],
+      eMoneyNumber: ['', [Validators.minLength(9)]],
+      eMoneyPin: ['', [Validators.minLength(4)]],
+    }),
+  });
+
+  constructor() {
+    // Dynamically set required validators for eMoney fields based on payment method
+    const paymentDetails = this.checkoutForm.get('paymentDetails') as FormGroup;
+    const methodControl = paymentDetails.get('method');
+    const eMoneyNumberControl = paymentDetails.get('eMoneyNumber');
+    const eMoneyPinControl = paymentDetails.get('eMoneyPin');
+
+    methodControl?.valueChanges.subscribe((method: string) => {
+      if (method === 'e-money') {
+        eMoneyNumberControl?.addValidators([
+          Validators.required,
+          Validators.minLength(9),
+        ]);
+        eMoneyPinControl?.addValidators([
+          Validators.required,
+          Validators.minLength(4),
+        ]);
+      } else {
+        eMoneyNumberControl?.removeValidators(Validators.required);
+        eMoneyPinControl?.removeValidators(Validators.required);
+      }
+      eMoneyNumberControl?.updateValueAndValidity();
+      eMoneyPinControl?.updateValueAndValidity();
+    });
+  }
+
+  submitCheckoutForm() {
+    console.log('submitting checkout details');
+    if (this.checkoutForm.invalid) {
+      this.checkoutForm.markAllAsTouched();
+      return
+    }
+
+    console.log(this.checkoutForm.value);
+  }
 }
