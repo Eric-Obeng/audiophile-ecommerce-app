32fa4c6cc6c8f3ca9b4c651873bfb90c9ca47b2d
Eric-Obeng <obengeric.3958@gmail.com>
2025-07-31 11:11:48 +0000

    feat: add ConfirmCheckout component with template and styles

---
+++ src/app/shared/components/confirm-checkout/confirm-checkout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-checkout',
+  imports: [],
+  templateUrl: './confirm-checkout.html',
+  styleUrl: './confirm-checkout.sass'
+})
+export class ConfirmCheckout {
+
+}
